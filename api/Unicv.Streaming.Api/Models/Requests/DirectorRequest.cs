3d8f146cb46c45bd95f81d44ef5bfaf105a55cf6
Pedro Yasui <pedro.yasui@bitzen.com.br>
2024-04-11 08:20:07 -0300

    crud de diretor

---
+++ api/Unicv.Streaming.Api/Models/Requests/DirectorRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Unicv.Streaming.Api.Models.Requests;
+
+public class DirectorRequest
+{
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Profile { get; set; }
+
+    public DateTime? BirthDate { get; set; }
+}
